cc3bd4decc8b5630bee35799bd6296d579e9d5dd
cjshaw <cjshaw@mit.edu>
2016-05-04 08:49:05 -0400

    change ckeditor config to use /touchstone url (to match future qbank format)

--- modules/utilities/ConfigureCKEditor.js
+++ modules/utilities/ConfigureCKEditor.js
@@ -1,7 +1,4 @@
 // ConfigureCKEditor.js
-// the cookie getting parts from:
-// From: https://docs.djangoproject.com/en/1.6/ref/contrib/csrf/
-// And:  http://stackoverflow.com/questions/6506897/csrf-token-missing-or-incorrect-while-post-parameter-via-ajax-in-django
 
 'use strict';
 
@@ -11,7 +8,7 @@ let ConfigureCKEditor = function (editor, repositoryId) {
     let MiddlewareService = require('../services/middleware.service.js');
 
     editor.config.extraPlugins = 'uploadimage';
-    editor.config.filebrowserUploadUrl = MiddlewareService.host() + '/repository/repositories/' + repositoryId + '/assets';
+    editor.config.filebrowserUploadUrl = MiddlewareService.secureHost() + '/repository/repositories/' + repositoryId + '/assets';
     editor.config.mathJaxLib = '//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML';
 };
 
