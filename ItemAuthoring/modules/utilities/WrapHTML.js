6157e60add89c5e93beb7d0b0e273e76a3f94e4d
cjshaw <cjshaw@mit.edu>
2016-05-26 11:27:31 -0400

    forcing mathjax to render everything

--- ItemAuthoring/modules/utilities/WrapHTML.js
+++ ItemAuthoring/modules/utilities/WrapHTML.js
@@ -3,22 +3,22 @@
 
 var WrapHTML = function (str) {
     var wrappedStr;
-    if (str.indexOf("math-tex") >= 0) {
-        wrappedStr = '<html>' +
-            '<head>' +
-            '<script type="text/javascript" src="//cdn.mathjax.org/mathjax/latest/MathJax.js?config=default"></script>' +
-            '<style>body * {margin:0px;padding:4px;}</style>' +
-            '</head>' +
-            '<body style="margin:0px;">' + str + '</body>' +
-            '</html>';
-    } else {
-        wrappedStr = '<html>' +
-            '<head>' +
-            '<style>body * {margin:0px;padding:4px;}</style>' +
-            '</head>' +
-            '<body style="margin:0px;">' + str + '</body>' +
-            '</html>';
-    }
+//    if (str.indexOf("math-tex") >= 0) {
+    wrappedStr = '<html>' +
+        '<head>' +
+        '<script type="text/javascript" src="//cdn.mathjax.org/mathjax/latest/MathJax.js?config=default"></script>' +
+        '<style>body * {margin:0px;padding:4px;}</style>' +
+        '</head>' +
+        '<body style="margin:0px;">' + str + '</body>' +
+        '</html>';
+//    } else {
+//        wrappedStr = '<html>' +
+//            '<head>' +
+//            '<style>body * {margin:0px;padding:4px;}</style>' +
+//            '</head>' +
+//            '<body style="margin:0px;">' + str + '</body>' +
+//            '</html>';
+//    }
 
     return wrappedStr;
 };
