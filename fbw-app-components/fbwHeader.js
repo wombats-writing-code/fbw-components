1fdd8fc760b1328e655e13b72213508c20844b2a
luwen-huang <luwenh@mit.edu>
2016-07-02 18:32:16 +1200

    added app components to be shared across both apps

---
+++ fbw-app-components/fbwHeader.js
@@ -0,0 +1,64 @@
+
+
+import React, {Component} from "react";
+import {View, StyleSheet, TouchableHighlight} from "react-native";
+
+/**
+  props = {
+    shouldShowMenuIcon: Boolean,
+    onPressMenuIcon: function,
+  }
+  
+*/
+export default class fbwHeader extends Component {
+
+    render() {
+
+      var menuIcon, rightIcon;
+      if (this.props.showShowMenuIcon) {
+        menuIcon = <TouchableHighlight onPress={() => this.props.onPressMenuIcon()}>
+                      <Image source={require('./assets/menu-icon.png')} styles={styles.menuIcon}/>
+                  </TouchableHighlight>
+      }
+
+      return (
+        <View style={styles.headerContainer}>
+
+          {menuIcon}
+
+          <View>
+            <Text style={styles.title}>{(this.props.title || '').toUpperCase()}</Text>
+            <Text style={styles.subTitle}>{(this.props.subTitle || '').toUpperCase()}</Text>
+          </View>
+
+          {rightIcon}
+
+        </View>
+      )
+
+    }
+
+    const styles = StyleSheet.create({
+      headerContainer: {
+        alignItems: 'center',
+        flexWrap: 'nowrap',
+        flexDirection: 'row',
+        background: '#fff',
+        paddingBottom: 12,
+        paddingTop: 30,            // so we don't hit the phone controls
+        marginBottom: 18,
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+      },
+      menuIcon: {
+      },
+      title: {
+        fontWeight: "700",
+        textAlign: 'center',
+      },
+      subTitle: {
+        textAlign: 'center',
+        color: '#999'
+      },
+    })
+}
